37cdc099526670c3cb7235f2408d6917c2771836
Arthur De Witte <arthur.dewitte@gmail.com>
2020-01-16 22:36:19 +0100

    Lol didn't add the main file ¯\_(ツ)_/¯

---
+++ API wrappers/mySe/mySE.py
@@ -0,0 +1,43 @@
+from requests import get, post
+from json import loads
+import error, logs
+
+
+def get_token(url: str, email: str):
+    """Forgot your token?
+    Fetch it using this function."""
+    fetch = get(url + 'token/', data={'mail': email})
+    if fetch.status_code == 401: raise error.UnauthorizedError("An invalid mail was given on `get_token`!")
+    else: return loads(fetch.text)["token"]
+
+
+def gen_token(url: str, email: str):
+    """Generate your unique auth token!"""
+    fetch = post(url + 'token/', data={'mail': email})
+    if fetch.status_code == 401: raise error.UnauthorizedError("An invalid mail was given on `gen_token`!")
+    print(fetch.text)
+    else: return loads(fetch.text)["token"]
+
+
+class Client:
+    def __init__(self, url, token):
+        self.url = url
+        self.token = None
+        self.selected = get(url + "logs/", headers={'Authorization': token})
+        if self.selected.status_code == 401: raise error.UnauthorizedError("An invalid token was given on setup!")
+
+    @property
+    def id(self):
+        """Returns the auth token its linked client id."""
+        return loads(self.selected.text)["id"]
+
+    @property
+    def logs(self):
+        """Fetches all linked log objects."""
+        selected = get(self.url + "logs/", headers={'Authorization': self.token})
+        return [logs.Log(log) for log in loads(selected.text)["logs"]]
+
+    def add_log(self, light: bool):
+        """Add a log to the db."""
+        selected = post(self.url + "logs/", data={'light': light}, headers={'Authorization': self.token})
+        return [logs.Log(log) for log in loads(selected.text)["logs"]]
