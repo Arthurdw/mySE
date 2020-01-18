3b448b07e38f3dd262a00799c583994ceed81cff
Arthur De Witte <arthur.dewitte@gmail.com>
2020-01-18 20:57:19 +0100

    PiPy

--- API wrappers/mySe/logs.py
+++ API wrappers/mySe/logs.py
@@ -1,3 +1,15 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# API wrapper created for mySE by Arthurdw                #
+# mySE stands for My Secure Environment!                  #
+# mySE was created for GO-Atheneum Oudenaarde             #
+# This project and all it files are under a MIT licence.  #
+# Project (mySE) started on 09/01/2020!                   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Log object for API wrapper.                             #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 from json import loads
 from datetime import datetime
 
--- API wrappers/mySe/mySE.py
+++ API wrappers/mySe/mySE.py
@@ -1,3 +1,15 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# API wrapper created for mySE by Arthurdw                #
+# mySE stands for My Secure Environment!                  #
+# mySE was created for GO-Atheneum Oudenaarde             #
+# This project and all it files are under a MIT licence.  #
+# Project (mySE) started on 09/01/2020!                   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Main API wrapper file                                   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
 from requests import get, post
 from json import loads
 import error, logs
