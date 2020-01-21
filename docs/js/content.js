3cc7f763764c1e0b94f876394f0bd20a36e604a7
Arthur De Witte <arthur.dewitte@gmail.com>
2020-01-21 21:26:42 +0100

    Finished Python API docs. (Atleast I think I did ¯\_(ツ)_/¯)

--- docs/js/content.js
+++ docs/js/content.js
@@ -5,20 +5,25 @@ for (var i = 0; i < titles.length; ++i) {
     var selectedElement = titles[i];
     selectedElement.href = '#' + selectedElement.id;
     selectedElement.title = "Get the permanent link.";
-    selectedElement.addEventListener("mouseover", function () { showURL(selectedElement); });
     selectedElement.addEventListener("mouseout", function () { clearURL(); });
 }
 
 function showURL(element) {
+    var whiteSpace = document.createElement("span");
+    whiteSpace.innerHTML = " ";
+    whiteSpace.classList.add("whiteSpace")
     var sub_el = document.createElement('i');
     sub_el.classList.add("im", "im-copy", "temp-ico")
+    element.append(whiteSpace);
     element.append(sub_el);
 }
 
 function clearURL() {
-    var i_list = document.getElementsByClassName("temp-ico")
-    for (var i = 0; i < i_list.length; ++i) {
-        i_list[i].remove();
+    removeAll("temp-ico");
+    removeAll("whiteSpace");
+    function removeAll(item) {
+        var itemList = document.getElementsByClassName(item);
+        for (var i = 0; i < itemList.length; ++i) { itemList[i].remove(); }
     }
 }
 
