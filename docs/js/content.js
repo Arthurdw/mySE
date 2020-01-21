9b3a98a18f1460bba6b433ff442bfbaac6e32eac
Arthur De Witte <arthur.dewitte@gmail.com>
2020-01-21 19:35:11 +0100

    Fixed the JS bugg/problem

--- docs/js/content.js
+++ docs/js/content.js
@@ -1,21 +1,25 @@
+var t0 = Date.now(); console.log("Started loading content JS.")
+
 var titles = document.getElementsByClassName("title");
 for (var i = 0; i < titles.length; ++i) {
     var selectedElement = titles[i];
-    selectedElement.href = window.location.href + '#' + selectedElement.id;
+    selectedElement.href = '#' + selectedElement.id;
+    selectedElement.title = "Get the permanent link.";
     selectedElement.addEventListener("mouseover", function () { showURL(selectedElement); });
-    selectedElement.addEventListener("onclick", function () { copyURL(selectedElement); });
-    selectedElement.addEventListener("mouseout", function () { clearURL(selectedElement); });
+    selectedElement.addEventListener("mouseout", function () { clearURL(); });
 }
 
 function showURL(element) {
     var sub_el = document.createElement('i');
     sub_el.classList.add("im", "im-copy", "temp-ico")
-    var el = element.append(sub_el);
+    element.append(sub_el);
 }
 
-function clearURL(element) {
+function clearURL() {
     var i_list = document.getElementsByClassName("temp-ico")
     for (var i = 0; i < i_list.length; ++i) {
         i_list[i].remove();
     }
 }
+
+var t1 = Date.now(); console.log("It took " + Math.round((t1 - t0) * 100) / 100 + " millisecond(s) to load the content JS file.")
