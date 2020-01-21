9267aae60e48048fdea7155599af5c9869edf8bd
Arthur De Witte <arthur.dewitte@gmail.com>
2020-01-21 19:37:56 +0100

    IDK y but it did some weird stuff. Fixed that lol...

--- docs/js/content.js
+++ docs/js/content.js
@@ -6,11 +6,7 @@ for (var i = 0; i < titles.length; ++i) {
     selectedElement.href = '#' + selectedElement.id;
     selectedElement.title = "Get the permanent link.";
     selectedElement.addEventListener("mouseover", function () { showURL(selectedElement); });
-<<<<<<< HEAD
     selectedElement.addEventListener("mouseout", function () { clearURL(); });
-=======
-    selectedElement.addEventListener("mouseout", function () { clearURL(selectedElement); });
->>>>>>> 0379c86e30d86dbf8c4ee029f0c38e247929e6f7
 }
 
 function showURL(element) {
@@ -25,8 +21,5 @@ function clearURL() {
         i_list[i].remove();
     }
 }
-<<<<<<< HEAD
 
 var t1 = Date.now(); console.log("It took " + Math.round((t1 - t0) * 100) / 100 + " millisecond(s) to load the content JS file.")
-=======
->>>>>>> 0379c86e30d86dbf8c4ee029f0c38e247929e6f7
